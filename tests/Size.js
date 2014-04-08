580fae1410d8e851cc8de1398a46c699bab861bd
dvdrtrgn <dvdrtrgn@gmail.com>
2014-04-08 00:21:45 -0500

    spruce up a bit

--- tests/Size.js
+++ tests/Size.js
@@ -4,8 +4,8 @@ var Size = function (w, h) {
     this.h = isNaN(h) ? '?' : h;
 };
 Size.prototype.toString = function () {
-    var w = this.round2(this.w);
-    var h = this.round2(this.h);
+    var w = this.round0(this.w);
+    var h = this.round0(this.h);
     return [w, h].join(' × ');
 };
 Size.prototype.round2 = function (x, y) {
@@ -14,5 +14,11 @@ Size.prototype.round2 = function (x, y) {
     }
     y = Math.pow(10, parseInt(y || 2));
     return Math.round(x * y) / y;
+};
+Size.prototype.round0 = function (x) {
+    if (typeof x !== 'number') {
+        return x;
+    }
+    return (x + 0.5) | 0;
 };
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
